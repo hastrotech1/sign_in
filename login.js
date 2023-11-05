2585a1c325ade6d34deacf853c5a4034d952d8fa
Maryannchiamaka <hastrotech1>
2023-11-05 23:49:59 +0100

    check and correct

---
+++ login.js
@@ -0,0 +1,12 @@
+const fullnameInput = document.getElementById('fullname');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+
+const submitButton = document.querySelector('button[type="submit"]');
+
+submitButton.addEventListener('click', function() {
+  // Validate the user input
+
+  // Submit the form
+});
